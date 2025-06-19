39570eaed06f0b62049f16e70a1a3f734ee05c1f
BlueSheep <48321445+BlueSheep2804@users.noreply.github.com>
2025-06-19 21:45:35 +0900

    refactor: 💡 バージョン毎の差異がある部分をまとめた

--- src/main/kotlin/io/github/bluesheep2804/togglevisualize/ToggleVisualize.kt
+++ src/main/kotlin/io/github/bluesheep2804/togglevisualize/ToggleVisualize.kt
@@ -14,103 +14,101 @@ import net.minecraft.resources.ResourceLocation
 import org.slf4j.LoggerFactory
 
 object ToggleVisualize : ClientModInitializer {
-	val logger = LoggerFactory.getLogger("togglevisualize")
-	//? if <1.21 {
-	/*val sprintOverlayTexture = ResourceLocation("minecraft:textures/mob_effect/speed.png")
-	val sneakOverlayTexture = ResourceLocation("minecraft:textures/item/hopper.png")
-	val flyingOverlayTexture = ResourceLocation("minecraft:textures/item/elytra.png")
-	*///?} else {
-	val sprintOverlayTexture: ResourceLocation = ResourceLocation.withDefaultNamespace("textures/mob_effect/speed.png")
-	val sneakOverlayTexture: ResourceLocation = ResourceLocation.withDefaultNamespace("textures/item/hopper.png")
-	val flyingOverlayTexture: ResourceLocation = ResourceLocation.withDefaultNamespace("textures/item/elytra.png")
-	//?}
-	private lateinit var config: ToggleVisualizeConfig
+    val logger = LoggerFactory.getLogger("togglevisualize")
+    val sprintOverlayTexture: ResourceLocation = rl("textures/mob_effect/speed.png")
+    val sneakOverlayTexture: ResourceLocation = rl("textures/item/hopper.png")
+    val flyingOverlayTexture: ResourceLocation = rl("textures/item/elytra.png")
+    lateinit var config: ToggleVisualizeConfig
 
-	override fun onInitializeClient() {
-		ToggleVisualizeConfig.load()
-		config = ToggleVisualizeConfig.instance
-		HudRenderCallback.EVENT.register { guiGraphics: GuiGraphics, _: /*? if <1.21 {*//*Float*//*?} else {*/ DeltaTracker /*?}*/ ->
-			val minecraftInstance = Minecraft.getInstance()
-			val options = minecraftInstance.options
-			//? if <1.20.2 {
-			/*val debugScreen = options.renderDebug
-			*///?} else {
-			val debugScreen = minecraftInstance.debugOverlay.showDebugScreen()
-			//?}
-			val hideGui = options.hideGui
-			if (debugScreen || hideGui || minecraftInstance.screen is PositioningScreen) return@register
+    override fun onInitializeClient() {
+        ToggleVisualizeConfig.load()
+        config = ToggleVisualizeConfig.instance
+        HudRenderCallback.EVENT.register { guiGraphics: GuiGraphics, _: /*? if <1.21 {*//*Float*//*?} else {*/ DeltaTracker /*?}*/ ->
+            val minecraftInstance = Minecraft.getInstance()
+            val options = minecraftInstance.options
+            //? if <1.20.2 {
+            /*val debugScreen = options.renderDebug
+            *///?} else {
+            val debugScreen = minecraftInstance.debugOverlay.showDebugScreen()
+            //?}
+            val hideGui = options.hideGui
+            if (debugScreen || hideGui || minecraftInstance.screen is PositioningScreen) return@register
 
-			val optionToggleSprint = options.toggleSprint().get()
-			val isSprintDown = options.keySprint.isDown
-			if (optionToggleSprint && isSprintDown) {
-				if (config.sprintShow) {
-					//? if <1.21.2 {
-					/*guiGraphics.blit(sprintOverlayTexture, config.sprintPositionX, config.sprintPositionY, 0F, 0F, 16, 16, 16, 16)
-					*///?} else {
-					guiGraphics.blit(RenderType::guiTexturedOverlay, sprintOverlayTexture, config.sprintPositionX, config.sprintPositionY, 0F, 0F, 16, 16, 16, 16)
-					//?}
-				}
-				if (config.sprintShowText) {
-					guiGraphics.drawString(minecraftInstance.font, Component.translatable("key.sprint").withStyle(ChatFormatting.ITALIC, ChatFormatting.GRAY), config.sprintTextPositionX, config.sprintTextPositionY, 16777215)
-				}
-			}
+            val optionToggleSprint = options.toggleSprint().get()
+            val isSprintDown = options.keySprint.isDown
+            if (optionToggleSprint && isSprintDown) {
+                if (config.sprintShow) {
+                    blit(guiGraphics, sprintOverlayTexture, config.sprintPositionX, config.sprintPositionY)
+                }
+                if (config.sprintShowText) {
+                    guiGraphics.drawString(minecraftInstance.font, Component.translatable("key.sprint").withStyle(ChatFormatting.ITALIC, ChatFormatting.GRAY), config.sprintTextPositionX, config.sprintTextPositionY, 16777215)
+                }
+            }
 
-			val optionToggleSneak = options.toggleCrouch().get()
-			val isSneakDown = options.keyShift.isDown
-			if (optionToggleSneak && isSneakDown) {
-				if (config.sneakShow) {
-					//? if <1.21.2 {
-					/*guiGraphics.blit(sneakOverlayTexture, config.sneakPositionX, config.sneakPositionY, 0F, 0F, 16, 16, 16, 16)
-					*///?} else {
-					guiGraphics.blit(RenderType::guiTexturedOverlay, sneakOverlayTexture, config.sneakPositionX, config.sneakPositionY, 0F, 0F, 16, 16, 16, 16)
-					//?}
-				}
-				if (config.sneakShowText) {
-					guiGraphics.drawString(minecraftInstance.font, Component.translatable("key.sneak").withStyle(ChatFormatting.ITALIC, ChatFormatting.GRAY), config.sneakTextPositionX, config.sneakTextPositionY, 16777215)
-				}
-			}
+            val optionToggleSneak = options.toggleCrouch().get()
+            val isSneakDown = options.keyShift.isDown
+            if (optionToggleSneak && isSneakDown) {
+                if (config.sneakShow) {
+                    blit(guiGraphics, sneakOverlayTexture, config.sneakPositionX, config.sneakPositionY)
+                }
+                if (config.sneakShowText) {
+                    guiGraphics.drawString(minecraftInstance.font, Component.translatable("key.sneak").withStyle(ChatFormatting.ITALIC, ChatFormatting.GRAY), config.sneakTextPositionX, config.sneakTextPositionY, 16777215)
+                }
+            }
 
-			val player = minecraftInstance.player
-			val isFlying = player?.isFallFlying
-			if (isFlying == true) {
-				if (config.flyingShow) {
-					//? if <1.21.2 {
-					/*guiGraphics.blit(
-						flyingOverlayTexture,
-						config.flyingPositionX,
-						config.flyingPositionY,
-						0F,
-						0F,
-						16,
-						16,
-						16,
-						16
-					)
-					*///?} else {
-					guiGraphics.blit(
-						RenderType::guiTexturedOverlay,
-						flyingOverlayTexture,
-						config.flyingPositionX,
-						config.flyingPositionY,
-						0F,
-						0F,
-						16,
-						16,
-						16,
-						16
-					)
-					//?}
-				}
-				if (config.flyingShowText) {
-					guiGraphics.drawString(
-						minecraftInstance.font,
-						Component.translatable("item.minecraft.elytra").withStyle(ChatFormatting.ITALIC, ChatFormatting.GRAY),
-						config.flyingTextPositionX,
-						config.flyingTextPositionY,
-						16777215
-					)
-				}
-			}
-		}
-	}
+            val player = minecraftInstance.player
+            val isFlying = player?.isFallFlying
+            if (isFlying == true) {
+                if (config.flyingShow) {
+                    blit(guiGraphics, flyingOverlayTexture, config.flyingPositionX, config.flyingPositionY)
+                }
+                if (config.flyingShowText) {
+                    guiGraphics.drawString(
+                        minecraftInstance.font,
+                        Component.translatable("item.minecraft.elytra").withStyle(ChatFormatting.ITALIC, ChatFormatting.GRAY),
+                        config.flyingTextPositionX,
+                        config.flyingTextPositionY,
+                        16777215
+                    )
+                }
+            }
+        }
+    }
+
+    fun rl(path: String): ResourceLocation {
+        //? if <1.21 {
+        /*return ResourceLocation("minecraft", path)
+        *///?} else {
+        return ResourceLocation.withDefaultNamespace(path)
+        //?}
+    }
+
+    fun blit(guiGraphics: GuiGraphics, texture: ResourceLocation, x: Int, y: Int) {
+        //? if <1.21.2 {
+        /*guiGraphics.blit(
+            texture,
+            x,
+            y,
+            0F,
+            0F,
+            16,
+            16,
+            16,
+            16
+        )
+        *///?} else {
+        guiGraphics.blit(
+            RenderType::guiTexturedOverlay,
+            texture,
+            x,
+            y,
+            0F,
+            0F,
+            16,
+            16,
+            16,
+            16
+        )
+        //?}
+    }
 }

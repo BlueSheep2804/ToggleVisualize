f88293d70e262af767ff6ef3c2707d49a22db3fc
BlueSheep <48321445+BlueSheep2804@users.noreply.github.com>
2025-09-09 20:30:36 +0900

    refactor: 💡 不必要な記述を削除

--- src/main/kotlin/io/github/bluesheep2804/togglevisualize/forge/ToggleVisualizeForge.kt
+++ src/main/kotlin/io/github/bluesheep2804/togglevisualize/forge/ToggleVisualizeForge.kt
@@ -6,7 +6,6 @@ import io.github.bluesheep2804.togglevisualize.common.ToggleVisualizeConfig
 import net.minecraftforge.api.distmarker.Dist
 import net.minecraftforge.client.ConfigScreenHandler.ConfigScreenFactory
 import net.minecraftforge.client.event.RegisterGuiOverlaysEvent
-import net.minecraftforge.common.MinecraftForge
 import net.minecraftforge.eventbus.api.SubscribeEvent
 import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.loading.FMLPaths
@@ -16,12 +15,8 @@ import thedarkcolour.kotlinforforge.forge.LOADING_CONTEXT
 @Mod.EventBusSubscriber(modid = ToggleVisualize.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = [Dist.CLIENT])
 object ToggleVisualizeForge {
     init {
-        MinecraftForge.EVENT_BUS.register(this)
         ToggleVisualize.init(FMLPaths.CONFIGDIR.get())
-        ConfigScreenFactory { parentScreen ->
-            ToggleVisualizeConfig.Companion.configScreen(parentScreen).generateScreen(parentScreen)
-        }
-        LOADING_CONTEXT.registerExtensionPoint<ConfigScreenFactory>(ConfigScreenFactory::class.java) {
+        LOADING_CONTEXT.registerExtensionPoint(ConfigScreenFactory::class.java) {
             ConfigScreenFactory { parentScreen ->
                 ToggleVisualizeConfig.Companion.configScreen(parentScreen).generateScreen(parentScreen)
             }
